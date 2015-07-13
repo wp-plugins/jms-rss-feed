32e796e4cd65d7df7e2df6e2f0977594d1ce3f15
james.liu <james.liu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 15:47:07 +0000

    Update the GCM feature
    git-svn-id: https://plugins.svn.wordpress.org/jms-rss-feed/trunk@1198120 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jms-rss-feed.php
+++ jms-rss-feed.php
@@ -12,6 +12,7 @@ Author URI: http://jmsliu.com/
 	$jms_rss_db_version = '1.0';
 
 	add_action('rss2_item', 'add_jms_img_rss_node');
+	add_action( 'publish_post', 'send_push_notification', 10, 2 );
 	
 	//work together
 	add_filter('query_vars', 'add_jms_single_post_var');
@@ -32,12 +33,74 @@ Author URI: http://jmsliu.com/
 		$vars[] = 'jms_rss_post_url';
 		$vars[] = 'jms_rss_action';
 		$vars[] = 'jms_rss_token';
+		$vars[] = 'jms_rss_send';
 		return $vars;
+	}
+	
+	function send_push_notification($ID, $post) {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'jms_rss_android_listener';
+		$devicesList = $wpdb->get_results("SELECT * FROM $table_name WHERE state=1", "ARRAY_A");
+		$wpdb->flush();
+		
+		if ( $devicesList )	{
+			
+			define("GOOGLE_API_KEY", "AIzaSyDYVTahqUmT4RX6brZnReUr6Rq2KNEUv90");
+			
+			$deviceList = array();
+			
+			foreach ( $devicesList as $device )	{
+				$token = $device["device_token"];
+				if(strlen($token) > 10) {
+					$deviceList[] = $token;
+				}
+			}
+			
+			//$author = $post->post_author; //Post author ID
+			//$name = get_the_author_meta( 'display_name', $author );
+			//$email = get_the_author_meta( 'user_email', $author );
+			$title = $post->post_title;
+			//$permalink = get_permalink( $ID );
+			//$edit = get_edit_post_link( $ID, '' );
+			
+			//$subject = sprintf( 'Published: %s', $title );
+			$message = sprintf ('The article “%s” has been published.', $title);
+			
+			//$message .= sprintf( 'View: %s', $permalink );
+			sendNotification($deviceList, $message);
+		}
 	}
 	
 	function return_jms_single_post($wp) {
 		global $wpdb;
 		
+		//this is for test
+		if(array_key_exists('jms_rss_send', $wp->query_vars)) {
+			$action = $wp->query_vars['jms_rss_send'];
+			if($action == '1') {
+				$table_name = $wpdb->prefix . 'jms_rss_android_listener';
+				$devicesList = $wpdb->get_results("SELECT * FROM $table_name WHERE state=1", "ARRAY_A");
+				$wpdb->flush();
+				
+				if ( $devicesList )	{
+					
+					define("GOOGLE_API_KEY", "AIzaSyDYVTahqUmT4RX6brZnReUr6Rq2KNEUv90");
+					
+					$deviceList = array();
+					
+					foreach ( $devicesList as $device )	{
+						$token = $device["device_token"];
+						if(strlen($token) > 10) {
+							$deviceList[] = $token;
+						}
+					}
+					
+					sendNotification($deviceList, "Hello World!");
+				}
+			}
+			
+			exit;
+		}
 		
 		if (array_key_exists('jms_rss_action', $wp->query_vars)) {
 			$action = $wp->query_vars['jms_rss_action'];
@@ -46,19 +109,19 @@ Author URI: http://jmsliu.com/
 				if($action == "register_android") {
 					$token = $wp->query_vars['jms_rss_token'];
 					$table_name = $wpdb->prefix . 'jms_rss_android_listener';
-					$wpdb->insert( 
-						$table_name, 
-						array(
-							'device_token' => $token, 
-							'state' => 1,
-							'reg_date' => current_time('mysql', 1)
-						),
-						array( 
-							'%s', 
-							'%d',
-							'%s'
-						)
-					);
+					
+					$wpdb->query( $wpdb->prepare( 
+						"
+							INSERT INTO $table_name
+							( device_token, state, reg_date )
+							VALUES ( %s, %d, %s )
+						", 
+							array(
+								$token, 
+								1, 
+								current_time('mysql', 1)
+							)
+					) );
 					
 					echo $wpdb->insert_id;
 					exit;
@@ -101,6 +164,45 @@ Author URI: http://jmsliu.com/
 			
 			add_option( "jms_rss_db_version", $jms_rss_db_version );
 		}
+	}
+	
+	function sendNotification($registrationIDs, $message) {
+		//$registrationIDs = array( "reg id1","reg id2");
+		$url = 'https://gcm-http.googleapis.com/gcm/send';
+		
+		$fields = array(
+			'registration_ids' => $registrationIDs,
+			'data' => array( "message" => $message ),
+		);
+		
+		$headers = array(
+			'Authorization: key=' . GOOGLE_API_KEY,
+			'Content-Type: application/json'
+		);
+		
+		// Open connection
+		$ch = curl_init();
+
+		// Set the URL, number of POST vars, POST data
+		curl_setopt( $ch, CURLOPT_URL, $url);
+		curl_setopt( $ch, CURLOPT_POST, true);
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+		//curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $fields));
+
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		// curl_setopt($ch, CURLOPT_POST, true);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $fields));
+
+		// Execute post
+		$result = curl_exec($ch);
+
+		// Close connection
+		curl_close($ch);
+		echo $result;
+		//print_r($result);
+		//var_dump($result);
 	}
 	
 ?>
